2c65a0e70a97c86df0ab058abb0734b08e78daf6
gustavo-cordova <gugugoedert@gmail.com>
2026-02-24 19:59:18 -0300

    Atv 4 concluída

--- aula-04/Quarto.java
+++ aula-04/Quarto.java
@@ -17,7 +17,7 @@ void main (){
     int [][] quadrado = {{1,2,3},{4,5,6},{7,8,9}};
 
         for(int i=0; i < quadrado.length; i++) {
-            for(int j=0; j < quadrado.length; j++) { 
+            for(int j=0; j < quadrado[i].length; j++) { 
 
                 if (i != j) {
 
