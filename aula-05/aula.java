49e559d0290c9533d53a8e16aa042a459609a822
gustavo-cordova <gugugoedert@gmail.com>
2026-03-02 08:10:37 -0300

    aula-05

---
+++ aula-05/aula.java
@@ -0,0 +1,16 @@
+void main () {
+
+    // String nome = "2.4"; // 'apóstrofo' e "aspas" ... "aspas duplas" 'aspas simples'
+    // int a = 10;
+    // a = interger.parseint(nome);
+    // double d = Double.parsedouble(nome);
+
+
+    // String nome = IO.readln("Entre com o seu nome: ");
+    // IO.println("Olá " + nome); 
+    // ou System.out.printf ("Olá %s", nome);
+    // ou IO.println (String.format("Olá $s, nome))";
+
+    
+
+}
