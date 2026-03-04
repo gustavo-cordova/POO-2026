876dbd4f6e3a8db35bb96fdf8267eca8ebeac9fb
gustavo-cordova <gugugoedert@gmail.com>
2026-03-04 11:08:36 -0300

    Teste com parâmetros

---
+++ aula-06/Argumentos.java
@@ -0,0 +1,20 @@
+public class Argumentos {
+    public static void main(String[] args) {
+                
+        // if (args.length < 2) {
+        //     System.out.println("ERRO:  Digite pelo menos dois parâmetros");
+        //     return;
+        // } 
+        
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+
+       
+
+        
+        
+        // String nome = IO.readln("Entre com seu nome: ");
+        // System.out.println("Bom dia " + nome);
+    }
+}
